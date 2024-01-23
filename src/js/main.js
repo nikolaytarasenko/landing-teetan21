8018a6e45e5d7b9ee081e56c1f7bd7f72f656f96
Mykola Tarasenko <kolyatarasenko@ukr.net>
2024-01-23 22:07:51 +0200

    add about, statistics, services sections

--- src/js/main.js
+++ src/js/main.js
@@ -2,37 +2,83 @@
 import AOS from 'aos';
 
 const initStickyHeader = e => {
-  const header = document.querySelector('.header');
-  const scrollY = window.scrollY;
+    const header = document.querySelector('.header');
+    const scrollY = window.scrollY;
 
-  header.classList.toggle('sticky', scrollY > 0);
-  document.body.classList.toggle('scrolled', scrollY > 0);
+    header.classList.toggle('sticky', scrollY > 0);
+    document.body.classList.toggle('scrolled', scrollY > 0);
 }
 
 const hidePreloader = () => {
-  const preloaderWrapper = document.querySelector('.preloader-wrapper');
+    const preloaderWrapper = document.querySelector('.preloader-wrapper');
 
-  preloaderWrapper.classList.add('hide');
+    preloaderWrapper.classList.add('hide');
 }
+
+// const initAnimatedNumbers = () => {
+//     const statisticsSection = document.querySelector('.statistics__list');
+//
+//     const statisticsObserver = new IntersectionObserver(
+//         (entries, observer) => {
+//             const [entry] = entries;
+//
+//             if (!entry.isIntersecting) return;
+//
+//             // animate number counter
+//             const counterNum = document.querySelectorAll('.statistics__item-number');
+//
+//             const speed = 30;
+//
+//             counterNum.forEach((curElem) => {
+//                 const updateNumber = () => {
+//                     const targetNumber = parseInt(curElem.dataset.number);
+//                     console.log('1 - target:', targetNumber);
+//                     const initialNum = parseInt(curElem.textContent);
+//                     console.log(typeof initialNum);
+//
+//                     const incrementNumber = Math.trunc(targetNumber / speed);
+//                     console.log('2 - incrementNumber:', incrementNumber);
+//
+//                     if (initialNum < targetNumber) {
+//                         console.log('yes')
+//                         curElem.textContent = `${initialNum + incrementNumber}+`;
+//                         setTimeout(updateNumber, 10);
+//                     }
+//                 };
+//
+//                 updateNumber();
+//             });
+//
+//             observer.unobserve(statisticsSection);
+//         },
+//         {
+//             root: null,
+//             threshold: 0,
+//         }
+//     );
+//
+//     statisticsObserver.observe(statisticsSection);
+// }
 
 const resizeHandler = () => {
 
 }
 
 const domContentLoadedHandler = () => {
-  window.addEventListener('scroll', initStickyHeader);
+    // initAnimatedNumbers()
+    window.addEventListener('scroll', initStickyHeader);
 }
 
 const loadHandler = () => {
-  AOS.init({
-    duration: 500,
-    easing: "ease-in-out",
-    once: true,
-    mirror: false,
-    disable: window.innerWidth < 768
-  });
+    AOS.init({
+        duration: 500,
+        easing: "ease-in-out",
+        once: true,
+        mirror: false,
+        disable: window.innerWidth < 768
+    });
 
-  hidePreloader();
+    hidePreloader();
 }
 
 window.addEventListener('resize', resizeHandler);
